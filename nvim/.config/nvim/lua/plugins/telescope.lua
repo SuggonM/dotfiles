e31f9c6c7d0858c5b364007d2849efa53e74f03d
Suggon <maharjansagun99@gmail.com>
2025-10-06 14:00:00 +0545

    + telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,10 +2,24 @@ return {
 	'nvim-telescope/telescope.nvim', branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
+		require('telescope').setup({
+			pickers = {
+				find_files = {
+					-- https://www.reddit.com/r/neovim/comments/nspg8o/comment/h0owlue/
+					find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
+				}
+			}
+		})
+
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<C-p>', builtin.find_files)
+		vim.keymap.set('n', '<C-p>',      builtin.find_files)
 		vim.keymap.set('n', '<leader>ff', builtin.find_files)
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep)
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+
+		-- quick access nvim configs from any location
+		vim.keymap.set('n', '<leader>nv', function()
+			builtin.find_files({ cwd = vim.fn.stdpath('config') })
+		end)
 	end
 }
