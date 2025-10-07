22bd6d3420976cc1edf5673343b2ca14fc309954
Suggon <maharjansagun99@gmail.com>
2025-10-07 10:21:05 +0545

    neotree plugin

---
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,34 @@
+return {{
+	'nvim-neo-tree/neo-tree.nvim',
+	branch = 'v3.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'MunifTanjim/nui.nvim',
+		'nvim-tree/nvim-web-devicons'
+	},
+	lazy = false,
+
+	config = function()
+		vim.keymap.set('n', '<leader>e', function()
+			vim.cmd('Neotree')
+		end)
+
+		require('neo-tree').setup({
+			close_if_last_window = true,
+			window = {
+				position = 'right',
+				width = '25%'
+			},
+			filesystem = {
+				hijack_netrw_behavior = 'open_current',
+				filtered_items = {
+					visible = true,
+					never_show = { '.git' }
+				}
+			},
+			default_component_configs = {
+				name = { trailing_slash = true }
+			}
+		})
+	end
+}}
