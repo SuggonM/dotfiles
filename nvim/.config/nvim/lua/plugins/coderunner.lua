2e370b122ce92d1ad983425a0a50f3b779afa676
Suggon <maharjansagun99@gmail.com>
2025-12-16 23:29:39 +0545

    add bullets for md lists and coderunner for programming

---
+++ nvim/.config/nvim/lua/plugins/coderunner.lua
@@ -0,0 +1,27 @@
+return {
+	'CRAG666/code_runner.nvim',
+
+	keys = {
+		{ '<leader>rr', vim.cmd.RunCode }
+	},
+
+	opts = {
+		before_run_filetype = vim.cmd.write,
+		filetype = {
+			java = 'java',
+			python = 'python3 -u'
+		}
+	},
+
+	config = function(_, opts)
+		local project_config = {}
+		local project_config_file = vim.fn.getcwd() .. '/.code_runner.lua'
+		local has_project_config = vim.fn.filereadable(project_config_file)
+		if has_project_config == 1 then
+			project_config = dofile(project_config_file)
+		end
+
+		local merged_config = vim.tbl_deep_extend('force', opts, project_config)
+		require('code_runner').setup(merged_config)
+	end
+}
