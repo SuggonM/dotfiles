195b70dfc3a357f53c001b7aea6e2a775b638ff8
Suggon <maharjansagun99@gmail.com>
2025-12-25 10:18:01 +0545

    nvim: disable smear cursor inside TTY
    smearcursor seemingly uses a set of vertical-bar characters with varying
    width to simulate smoothness. But TTY framebuffer doesn't have "fallback
    font" so it renders those unknown glyphs as diamond-shape characters,
    which quickly becomes distracting when you move the cursor

--- nvim/.config/nvim/lua/plugins/cursor.lua
+++ nvim/.config/nvim/lua/plugins/cursor.lua
@@ -2,6 +2,11 @@
 return {
 	'sphamba/smear-cursor.nvim',
 
+	-- disable inside TTY, it creates visual artifacts
+	cond = not (
+		vim.env.XDG_SESSION_TYPE == 'tty' or
+		vim.env.TERM == 'linux'
+	),
 	opts = {
 		stiffness = 0.5,
 		trailing_stiffness = 0.5,
