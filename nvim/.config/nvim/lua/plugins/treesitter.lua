9fa39706f256ef7cb4db465fab5c49a9bc9cf11f
Suggon <maharjansagun99@protonmail.com>
2026-03-16 20:28:09 +0545

    nvim: remove incremental selection keybinds already in neovim
    incremental selection is now in neovim 0.12
    new default keybinds are: [n, ]n, in, an
    
    ref:
    https://pawelgrzybek.com/nvim-incremental-selection/#update-20260309

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -7,15 +7,7 @@ return {
 		'MeanderingProgrammer/treesitter-modules.nvim',
 		opts = {
 			auto_install = true,
-			highlight = { enable = true },
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = ']v',
-					node_incremental = ']v',
-					node_decremental = '[v'
-				}
-			}
+			highlight = { enable = true }
 		}
 	}
 }
