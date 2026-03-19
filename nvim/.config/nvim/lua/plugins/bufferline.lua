92fbb73a8e669618a96114fbe6aa3be252022cec
Suggon <maharjansagun99@protonmail.com>
2026-03-19 18:01:14 +0545

    nvim: replace bufferline with stabline
    that plugin was a hot mess. i found an alternative that's less bloated
    and works with a single color declaration instead of... *looks at
    previous config* having to declare colors for every single corner and
    calling the plugin "adaptive to your colorscheme"

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,31 +1,23 @@
 ---@type LazySpec
 return {
-	'akinsho/bufferline.nvim',
-	version = '*',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-
-	opts = function()
-		local background = '#1d1f27'
-		local active_bg = '#111111'
-		local inactive_bg = '#17181c'
-
-		return {
-			options = {
-				style_preset = { 3, 4 },
-				separator_style = 'slope',
-				always_show_bufferline = false,
-				show_buffer_close_icons = false,
-				right_mouse_command = false
-			},
-			highlights = {
-				fill = { bg = background },
-				background = { bg = inactive_bg },
-				buffer_selected = { bg = active_bg },
-				buffer_visible = { bg = active_bg },
-				separator = { fg = background, bg = inactive_bg },
-				separator_visible = { fg = background },
-				separator_selected = { fg = background }
-			}
-		}
+	'tamton-aquib/staline.nvim',
+	config = function()
+		vim.api.nvim_create_autocmd('BufAdd', {
+			pattern = '*',
+			callback = function()
+				local bufcount = #vim.fn.getbufinfo({ buflisted = 1 })
+				vim.o.showtabline = (bufcount > 1) and 2 or 0
+			end
+		})
+		require('stabline').setup({
+			bg = '#111111',
+			stab_bg = '#1d1f27',
+			inactive_bg = '#17181c',
+			inactive_fg = '#888888',
+			style = 'slant',
+			stab_right = '',
+			stab_start = '  ',
+			font_active = 'none'
+		})
 	end
 }
