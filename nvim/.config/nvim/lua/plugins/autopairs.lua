2ca21db5ceec110ba82903d6ea454a9107736042
Suggon <maharjansagun99@protonmail.com>
2026-03-19 20:57:46 +0545

    nvim: set autoclose to autoindent tags
    also add full table for `'` and `"` because apparently autoclose doesn't
    use vim.tbl_deep_extend()?

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -14,8 +14,9 @@ return {
 			pair_spaces = true
 		},
 		keys = {
-			["'"] = { disable_command_mode = true },
-			['"'] = { disable_command_mode = true }
+			['>'] = { escape = false, close = false, pair = '><' },
+			["'"] = { escape = true, close = true, pair = "''", disable_command_mode = true },
+			['"'] = { escape = true, close = true, pair = '""', disable_command_mode = true }
 		}
 	}
 }
