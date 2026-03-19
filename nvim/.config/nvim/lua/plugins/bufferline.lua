fef9f826c9a3a010ec7e5c64e4dcfbe88b280491
Suggon <maharjansagun99@protonmail.com>
2026-03-19 18:11:53 +0545

    nvim: change stabline to my PR branch
    the PR fixes bugs and adds color to "buffer modified" icon which is
    essential to my workflow. the plugin author seems to be away(?) so it
    may not get merged quickly

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,7 +1,9 @@
 ---@type LazySpec
 return {
-	'tamton-aquib/staline.nvim',
-	config = function()
+	'SuggonM/staline.nvim',
+	main = 'stabline',
+
+	init = function()
 		vim.api.nvim_create_autocmd('BufAdd', {
 			pattern = '*',
 			callback = function()
@@ -9,15 +11,16 @@ return {
 				vim.o.showtabline = (bufcount > 1) and 2 or 0
 			end
 		})
-		require('stabline').setup({
-			bg = '#111111',
-			stab_bg = '#1d1f27',
-			inactive_bg = '#17181c',
-			inactive_fg = '#888888',
-			style = 'slant',
-			stab_right = '',
-			stab_start = '  ',
-			font_active = 'none'
-		})
-	end
+	end,
+
+	opts = {
+		bg = '#111111',
+		stab_bg = '#1d1f27',
+		inactive_bg = '#17181c',
+		inactive_fg = '#888888',
+		style = 'slant',
+		stab_right = '',
+		stab_start = '  ',
+		font_active = 'none'
+	}
 }
