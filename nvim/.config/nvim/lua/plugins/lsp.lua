2c88396565428d31fbe770f7fe626acb6d1650fa
Suggon <maharjansagun99@protonmail.com>
2026-03-19 20:15:08 +0545

    nvim: set keymap 'grh' to toggle inlay hints
    first increment selection, then documentcolor and now inlay hints. these
    neovim lsp features are crazy good

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -14,6 +14,12 @@ return {
 		vim.api.nvim_create_autocmd('LspAttach', {
 			callback = function()
 				vim.keymap.set('n', 'grf', vim.lsp.buf.format)
+				vim.keymap.set('n', 'grh', function()
+					vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+				end)
+				vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+				vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bg = '#525252' })
+				vim.lsp.document_color.enable(true, 0, { style = 'virtual' })
 				vim.diagnostic.config({
 					virtual_text = true,
 					signs = {
@@ -26,8 +32,6 @@ return {
 						}
 					}
 				})
-				vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-				vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', { bg = '#525252' })
 			end
 		})
 	}
