473b3d64718dd5be5f852237dbcc27e68cc20e3e
Suggon <maharjansagun99@protonmail.com>
2026-04-11 08:10:38 +0545

    SCRIPTS: replace microG gh releases with fdroid repo

--- SCRIPTS/waydroid-init.sh
+++ SCRIPTS/waydroid-init.sh
@@ -6,7 +6,7 @@ if [ $USER = "root" ]; then
 	exit 1
 fi
 
-# probably connect to a fast VPN first
+# if sourceforge is slow, connect to a VPN
 # https://github.com/waydroid/waydroid/issues/623#issuecomment-3864873879
 sudo pacman -S --needed waydroid
 paru -S --needed --noconfirm waydroid-image waydroid-script-git bindfs
@@ -15,17 +15,18 @@ paru -S --needed --noconfirm waydroid-image waydroid-script-git bindfs
 # sudo sed -i 's/if result.stderr:/if result.returncode != 0 and result.stderr:/' \
 # 	/opt/waydroid-script/tools/helper.py
 
-# sudo waydroid-extras install microg  # also installs playstore??
+# sleep needed? waydroid_script restarts the session on each command
 sudo waydroid-extras install libhoudini
 sudo waydroid-extras install magisk
 sudo waydroid shell pm disable com.android.inputmethod.latin
 sudo waydroid shell cmd media_session volume --set 15
 waydroid prop set persist.waydroid.multi_windows true
 
+# arch-only issue?
 # https://github.com/waydroid/waydroid/issues/143#issuecomment-1520857943
 sudo sed -i'~' -E 's/=.\$\(command -v (nft|ip6?tables-legacy).*/=/g' \
 	/usr/lib/waydroid/data/scripts/waydroid-net.sh
 
-# download microG and F-Droid
-# https://github.com/microg/GmsCore/releases/latest
-# https://f-droid.org/
+# download F-Droid and add microG
+# https://f-droid.org/F-Droid.apk
+# https://microg.org/fdroid/repo/?fingerprint=9BD06727E62796C0130EB6DAB39B73157451582CBD138E86C468ACC395D14165
