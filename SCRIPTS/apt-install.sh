788897f339af5c40ea3c0603ec13425707d184c6
Suggon <maharjansagun99@protonmail.com>
2026-04-20 19:40:45 +0545

    change configs to android/termux
    some are replaced, others are stripped down to absolute minimum

--- SCRIPTS/apt-install.sh
+++ SCRIPTS/apt-install.sh
@@ -2,86 +2,37 @@
 set -e
 
 if ! command -v fzf > /dev/null; then
-	sudo apt-get update
-	sudo apt-get install fzf
+	apt-get update
+	apt-get install fzf root-repo
 fi
 
 pkgs=(
-	adb
-	aha  # convert terminal output into html
-	anacron
-	ani-cli
-	aptitude
-	arch-install-scripts  # arch-chroot is too convenient
-	atool  # archive listing integration with `lf`
+	android-tools
 	bat
-	bindfs  # bind-mount with altered ownership
-	build-essential  # compile software from source
-	catimg
-	clifm
-	cmake
-	dconf-editor
-	default-jdk  # java JDK
-	docker.io
+	bindfs
 	fastfetch
-	fd-find  # `find` alternative, nvim-telescope dependency
-	ffmpeg
-	flatpak
-	font-manager
-	fprintd  # linux fingerprint interface
 	fzf
 	gh
 	git-delta
-	gnome-boxes
-	gnome-shell-extension-manager
-	gnome-software
-	gnome-software-plugin-flatpak
-	gnome-sushi  # quick file preview from Nautilus
-	gnome-tweaks  # set dark theme on legacy GTK apps
-	gparted
-	hexedit
-	ibus-mozc  # Japanese input
 	jq
-	keyd  # key remap
 	lazygit
 	lf
-	lolcat
-	lua5.4
-	lynx
-	mediainfo
-	mozc-utils-gui  # Mozc dependency
+	mlocate
 	ncdu
 	neovim
 	net-tools
 	nmap
-	openssh-server
-	pipx
-	plocate
-	postgresql
-	python3-pip
+	openssh
 	qalc
-	radare2  # disassembler
-	rar  # this is trial version btw :)
-	ripgrep  # nvim-telescope dependency
-	ruby
-	samba
-	scrcpy
+	root-repo
 	sl
-	speedtest-cli
+	speedtest-go
 	stow
-	systemd-zram-generator
-	tldr-py
+	tealdeer
 	tmux
 	traceroute
 	translate-shell
 	tree
-	tree-sitter-cli
-	vlc
-	wl-clipboard
-	woff2
-	x11-apps  # detect Xwayland with xeyes
-	xxd  # colorful hexdump
-	ydotool  # auto clicker
 	yq
 )
 
@@ -92,5 +43,5 @@ selected=(
 echo "Selected:"
 printf '%s\n' "${selected[@]}" | column
 
-sudo apt-get update
-sudo apt-get install "${selected[@]}"
+apt-get update
+apt-get install "${selected[@]}"
