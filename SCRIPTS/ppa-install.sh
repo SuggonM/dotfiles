931a2174aa8c4cb1c789d853cc1b4e6cb1aae197
Suggon <maharjansagun99@protonmail.com>
2026-05-25 18:44:26 +0545

    replace OBS with gpu screen recorder
    OBS feels more complex to use than gpuSR. it also supports global
    hotkeys

--- SCRIPTS/ppa-install.sh
+++ SCRIPTS/ppa-install.sh
@@ -3,7 +3,6 @@ set -e
 
 sudo add-apt-repository -y ppa:lakinduakash/lwh
 sudo add-apt-repository -y ppa:mozillateam/ppa
-sudo add-apt-repository -y ppa:obsproject/obs-studio
 
 # https://packagecloud.io/filips/FirefoxPWA
 curl -s https://packagecloud.io/install/repositories/filips/FirefoxPWA/script.deb.sh?any=true | sudo bash
@@ -15,7 +14,6 @@ pkgs=(
 	firefox
 	firefoxpwa
 	linux-wifi-hotspot
-	obs-studio
 	waydroid
 )
 
