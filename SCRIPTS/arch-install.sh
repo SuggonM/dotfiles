b3ebc93c1c7f07ac42182f74966358ede4d5a158
Suggon <maharjansagun99@protonmail.com>
2026-08-02 14:55:05 +0545

    arch-install: add missed font-viewer and keyring
    not sure how i missed these. not having gnome-keyring was the reason the
    wifi settings had no prompt for entering the password
    
    also include the power-manager to see battery report

--- SCRIPTS/arch-install.sh
+++ SCRIPTS/arch-install.sh
@@ -80,7 +80,7 @@ rm -r ~suggon/paru-alpm-bin/
 
 pacman -S --noconfirm pipewire wireplumber pipewire-pulse pipewire-jack
 pacman -S --noconfirm noto-fonts ttf-ubuntu-font-family ttf-input-nerd terminus-font
-pacman -S --noconfirm baobab gdm gnome-calculator gnome-characters gnome-clocks ptyxis gnome-control-center gnome-disk-utility gnome-logs gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell resources gst-thumbnailers gvfs gvfs-mtp gvfs-smb gvfs-wsdd loupe nautilus papers sushi xdg-desktop-portal-gnome xdg-user-dirs-gtk
+pacman -S --noconfirm baobab gdm gnome-calculator gnome-characters gnome-clocks ptyxis gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-logs gnome-remote-desktop gnome-session gnome-settings-daemon gnome-shell resources gst-thumbnailers gvfs gvfs-mtp gvfs-smb gvfs-wsdd loupe nautilus papers sushi xdg-desktop-portal-gnome xdg-user-dirs-gtk gnome-power-manager
 
 systemctl enable gdm bluetooth
 
